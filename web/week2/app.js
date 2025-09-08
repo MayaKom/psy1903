b4ee2260528c06fe5edc28d412f44e3b44634fc3
Maya Komakhidze <mayakomak@gmail.com>
2025-09-08 15:04:50 -0400

    Week 1 practice code

--- web/week2/app.js
+++ web/week2/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+let experiment = "Stroop";
+let trialCountMax = 20;
+console.log(typeof experiment); //string
+console.log(experiment.charAt(0)) // S
+// TODO: Randomize colors
+let colors = ['red', 'greem', 'blue'];
+alert('Welcome to the ' + experiment + " experiment")
+trialCountMax = 40;
+let halfWayfWayCount = trialCountMax / 2
+console.log(halfWayfWayCount); // Expected: 20
+
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 <= 15); //true
