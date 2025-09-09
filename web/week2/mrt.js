7ae40c4f5e76ad8f45dea74f379e3552786865d3
Maya Komakhidze <mayakomak@gmail.com>
2025-09-09 17:02:25 -0400

    Included response times in the experiment

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -1,8 +1,17 @@
 alert('In this experiment we will measure your response time.You will be shown a series of simple math equations.Answer these equations as quickly and accurately as you can')
+let start1 = new Date().getTime();
 let answer1 = prompt('what is 1 + 4?');
-alert('You answered ' + answer1);
+let end1 = new Date().getTime();
+let responseTime1 = (end1 - start1) / 1000;
+alert('You answered ' + answer1 + ' in ' + responseTime1 + ' seconds');
+let start2 = new Date().getTime();
 let answer2 = prompt('What is 3 + 5?');
-alert('You answered ' + answer2);
+let end2 = new Date().getTime();
+let responseTime2 = (end2 - start2) / 1000;
+alert('You answered ' + answer2 + ' in ' + responseTime2 + ' seconds');
+let start3 = new Date().getTime();
 let answer3 = prompt('What is 4+6?');
-alert('You answered ' + answer3);
+let end3 = new Date().getTime();
+let responseTime3 = (end3 - start3) / 1000;
+alert('You answered ' + answer3 + ' in ' + responseTime3 + ' seconds');
 alert('Thank you for your participation!');
