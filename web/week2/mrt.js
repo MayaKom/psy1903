206fda954a86d014cacc118fb630df265205ef5a
Maya Komakhidze <mayakomak@gmail.com>
2025-09-09 17:11:24 -0400

    Modified the experiment with randomly generated numbers

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -1,16 +1,19 @@
 alert('In this experiment we will measure your response time.You will be shown a series of simple math equations.Answer these equations as quickly and accurately as you can')
 let start1 = new Date().getTime();
-let answer1 = prompt('what is 1 + 4?');
+let answer1 = prompt('what is ' + (Math.floor(Math.random() * 10) + 1) + "+" + (Math.floor(Math.random() * 10) + 1));
+
 let end1 = new Date().getTime();
 let responseTime1 = (end1 - start1) / 1000;
 alert('You answered ' + answer1 + ' in ' + responseTime1 + ' seconds');
 let start2 = new Date().getTime();
-let answer2 = prompt('What is 3 + 5?');
+let answer2 = prompt('what is ' + (Math.floor(Math.random() * 10) + 1) + "+" + (Math.floor(Math.random() * 10) + 1));
+
 let end2 = new Date().getTime();
 let responseTime2 = (end2 - start2) / 1000;
 alert('You answered ' + answer2 + ' in ' + responseTime2 + ' seconds');
 let start3 = new Date().getTime();
-let answer3 = prompt('What is 4+6?');
+let answer3 = prompt('what is ' + (Math.floor(Math.random() * 10) + 1) + "+" + (Math.floor(Math.random() * 10) + 1));
+
 let end3 = new Date().getTime();
 let responseTime3 = (end3 - start3) / 1000;
 alert('You answered ' + answer3 + ' in ' + responseTime3 + ' seconds');
