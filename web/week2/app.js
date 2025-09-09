4b47ff1180e120ff5f5ad36fc5aa8b961c918af1
Maya Komakhidze <mayakomak@gmail.com>
2025-09-09 16:38:33 -0400

    Created a new file for math experiment equations

--- web/week2/app.js
+++ web/week2/app.js
@@ -1,15 +1,24 @@
-let experiment = "Stroop";
-let trialCountMax = 20;
-console.log(typeof experiment); //string
-console.log(experiment.charAt(0)) // S
-// TODO: Randomize colors
-let colors = ['red', 'greem', 'blue'];
-alert('Welcome to the ' + experiment + " experiment")
-trialCountMax = 40;
-let halfWayfWayCount = trialCountMax / 2
-console.log(halfWayfWayCount); // Expected: 20
+// let experiment = "Stroop";
+// let trialCountMax = 20;
+// console.log(typeof experiment); //string
+// console.log(experiment.charAt(0)) // S
+// // TODO: Randomize colors
+// let colors = ['red', 'greem', 'blue'];
+// alert('Welcome to the ' + experiment + " experiment")
+// trialCountMax = 40;
+// let halfWayfWayCount = trialCountMax / 2
+// console.log(halfWayfWayCount); // Expected: 20
 
-let correct = true;
-console.log(typeof correct);
+// let correct = true;
+// console.log(typeof correct);
 
-console.log(10 <= 15); //true
+// console.log(10 <= 15); //true
+
+
+console.log('Hello World');
+
+let courseName = 'Programming for Psychologists';
+console.log(courseName.toUpperCase()); //PROGRAMMING FOR PSYCHOLOGISTS
+console.log(courseName.indexOf("Psychologists")); //16
+console.log(courseName.replace("Programming", "Coding"));
+
---
+++ web/week2/mrt.js
@@ -0,0 +1,8 @@
+alert('In this experiment we will measure your response time.You will be shown a series of simple math equations.Answer these equations as quickly and accurately as you can')
+let answer1 = prompt('what is 1 + 4?');
+alert('You answered ' + answer1);
+let answer2 = prompt('What is 3 + 5?');
+alert('You answered ' + answer2);
+let answer3 = prompt('What is 4+6?');
+alert('You answered ' + answer3);
+alert('Thank you for your participation!');
