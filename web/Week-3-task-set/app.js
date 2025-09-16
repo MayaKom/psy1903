c8f84ee2959d924c2c734846657e631395c50391
Maya Komakhidze <mayakomak@gmail.com>
2025-09-16 16:18:22 -0400

    Created a math task with random numbers

--- web/Week-3-task-set/app.js
+++ web/Week-3-task-set/app.js
@@ -7,3 +7,38 @@ let randomNum2 = Math.floor(Math.random() * 10) + 1;
 num1.innerHTML = randomNum1;
 num2.innerHTML = randomNum2;
 
+/* let response = prompt('What is your name?');
+let count = response.length;
+console.log(count);
+let firstLetter = response.charAt(0)
+console.log(firstLetter)
+let lastLetter = response.charAt(count - 1);
+console.log(lastLetter)
+ */
+
+/* let a = 5;
+let b = 10;
+console.log(a > 3 && b < 15); */
+
+/* let over18 = false;
+let hasGuardianApproval = true;
+console.log(over18 || hasGuardianApproval); */
+
+/* let count = 8;
+console.log(count % 2 == 0); */
+
+
+let response = prompt('What is ' + randomNum1 + '+' + randomNum2 + '?');
+
+
+let feedback = '';
+
+if (response == (randomNum1 + randomNum2)) {
+    feedback = 'Correct!';
+} else if (response == (randomNum1 + randomNum2 - 1) || response == (randomNum1 + randomNum2 + 1)) {
+    feedback = 'You were close!';
+} else {
+    feedback = 'Incorrect.';
+}
+
+alert(feedback + ' The expected answer is ' + (randomNum1 + randomNum2));
