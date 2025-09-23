013f39ad00e3b42049da264e7e9cb8bd1980c8b9
Maya Komakhidze <mayakomak@gmail.com>
2025-09-23 16:32:31 -0400

    New function for two-way conversion

--- web/week-4-task-set/app.js
+++ web/week-4-task-set/app.js
@@ -1,5 +1,17 @@
-function celsiusToFahrenheit(C) {
+/* function celsiusToFahrenheit(C) {
     let F = (C * 1.8) + 32
     return F
 }
-console.log(celsiusToFahrenheit(40))
+console.log(celsiusToFahrenheit(40)) */
+
+function convertTemp(temp, convertTo) {
+    let degrees = 0
+    if (convertTo == 'F') {
+        degrees = (temp * 1.8) + 32
+    } else if (convertTo == 'C') {
+        degrees = (temp - 32) / 1.8
+    }
+    return degrees
+}
+
+console.log(convertTemp(104, 'C'))
