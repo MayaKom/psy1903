d0be63550099909fa35928e323e0af40e251020f
Maya Komakhidze <mayakomak@gmail.com>
2025-09-30 16:46:44 -0400

    Math equation form with 3 trials

--- web/week5-task-set/experiment.js
+++ web/week5-task-set/experiment.js
@@ -1,28 +1,53 @@
 let jsPsych = initJsPsych();
 let timeline = [];
 
+
+
+
 // Welcome
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-        <h1> Welcome to the Math Response Task </h1>
-        <p> In this experiment... </p>
-        <p>  </p>
-        <p> </p>
-        <p>  </p>
+        <h1> Welcome to the Math Response Task!</h1>
+        <p>In this experiment, you will be shown a series of math questions.</p>
+        <p>Please answer as quickly and accurately as possible.</p>
+        <p>Press SPACE to begin.</p>
     `,
     choices: [' '],
 };
 timeline.push(welcomeTrial)
 
+for (let condition of conditions) {
+    let conditionTrial = {
+        type: jsPsychSurveyHtmlForm,
+        preamble: `<p>What is ${condition.num1} + ${condition.num2}?</p>`,
+        html: `<p><input type='text' name='response' id='response'></p>`,
+        autofocus: 'response',
+        button_label: 'Submit Answer',
+        data: {
+            collect: true,
+            number1: condition.num1,
+            number2: condition.num2,
+            correctAnswer: condition.correctAnswer
+        },
+        on_finish: function (data) {
+            let userResponse = data.response.response;
+            if (userResponse == condition.correctAnswer) {
+                data.correct = true;
+            } else {
+                data.correct = false;
+            }
 
+        }
 
-
+    }
+    timeline.push(conditionTrial)
+}
 
 
 // Debrief
 let debriefTrial = {
-    type: jsPsychHtmlKeyboardResponse,
+    type: jsPsychSurveyHtmlForm,
     stimulus: `
     <h1>Thank you!</h1>
     <p>You can now close this tab.</p>
@@ -32,7 +57,7 @@ let debriefTrial = {
         let data = jsPsych.data
             .get()
             .filter({ collect: true })
-            .ignore(['response', 'stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
+            .ignore(['stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
             .csv();
         console.log(data);
     }
