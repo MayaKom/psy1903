e09740e5f98dabfd256709c8efd5e998d132a4ab
Maya Komakhidze <mayakomak@gmail.com>
2025-10-14 15:30:35 -0400

    Last updates

--- web/Project/experiment.js
+++ web/Project/experiment.js
@@ -98,12 +98,69 @@ let likertTrial = {
     type: jsPsychSurveyLikert,
     questions: questions,
     randomize_question_order: false,
-    data: { collect: true }
+    data: {
+        collect: true
+    }
+
 };
 
 
 timeline.push(likertTrial);
 
+
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: `
+    <h1> Please wait...</h1 >
+    <span class='loader'></span>
+    <p>We are saving the results of your inputs.</p>
+`,
+    on_start: function () {
+        //  ⭐ Update the following three values as appropriate ⭐
+        let prefix = 'uncertainty-tolerance';
+        let dataPipeExperimentId = 'OV8df8DU3zXE';
+        let forceOSFSave = true;
+
+        // Filter and retrieve results as CSV data
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'plugin_version', 'collect'])
+            .csv();
+
+        // Generate a participant ID based on the current timestamp
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        // Dynamically determine if the experiment is currently running locally or on production
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        // Send the results to our saving end point
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            jsPsych.finishTrial();
+        })
+    }
+}
+
+timeline.push(resultsTrial);
 
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
